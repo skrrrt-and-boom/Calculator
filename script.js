29e96c99c49236032cbc515a9e2395255c1cef30
skrrrt-and-boom <domin6051@gmail.com>
2022-06-05 19:47:58 +0200

    3 commit

--- script.js
+++ script.js
@@ -4,18 +4,21 @@ const c = document.getElementById("C");
 const footer = document.querySelector("footer");
 const scroon = document.querySelector(".screen");
 const mega = document.getElementsByClassName("mega");
+const equals = document.getElementById("=");
+const body = document.querySelector("body");
+const cal = document.querySelector(".calculator");
 
+let para = scroon.childNodes;
 let megaCounter = [];
 //  Color after & logic
 function fuckLogic() {
   for (let i = 0; i < button1.length; i++) {
-    button1[i].addEventListener("click", function display() {
+    button1[i].addEventListener("click", () => {
       button1[i].style.backgroundColor = "rgba(152, 8, 139, 0.3)";
       if (i !== 2 & i !== 3) {
       let clone = button1[i].firstChild.cloneNode(true);
       scroon.appendChild(clone);
       }
-      button1[i].removeEventListener("click", display);
     });
   }
 }
@@ -38,7 +41,6 @@ c.addEventListener("dblclick", () => {
     while (scroon.lastChild) {
       scroon.removeChild(scroon.lastChild);
     }
-    fuckLogic();
 });
 
 //  remove number & last color
@@ -56,6 +58,74 @@ c.addEventListener("click", () => {
   }
   mega[megaCounter.pop()].style.backgroundColor = "rgba(85,118,209,0)";
 });
+
+//  loops
+function loop1(i) {
+  let num1 = "";
+  for (let j = 0; j < i; j++) {
+    num1 += para[j].textContent;
+  }
+  return num1;
+}
+
+function loop2(i) {
+  let num2 = "";
+  for (let k = i + 1; k < para.length; k++) {
+    num2 += para[k].textContent;
+  }
+  return num2;
+}
+
+//  do the math
+function count() {
+  for (let i = 0; i < para.length; i++) {
+    if (para[i].textContent === "-") {
+      let aaa = loop1(i);
+      let bbb = loop2(i);
+      return Number(aaa) - Number(bbb);
+    }
+    else if (para[i].textContent === "+") {
+      let aaa = loop1(i);
+      let bbb = loop2(i);
+      return Number(aaa) + Number(bbb);
+    }
+    else if (para[i].textContent === "*") {
+      let aaa = loop1(i);
+      let bbb = loop2(i);
+      return Number(aaa) * Number(bbb);
+    }
+    else if (para[i].textContent === "÷") {
+      let aaa = loop1(i);
+      let bbb = loop2(i);
+      return Number(aaa) / Number(bbb);
+    }
+  }
+}
+
+equals.addEventListener("click", () => {
+  let opa = document.createElement("p");
+  opa.textContent = count();
+  while (scroon.lastChild) {
+    scroon.removeChild(scroon.lastChild);
+  }
+  scroon.appendChild(opa);
+  if (para[0].textContent === "Infinity" || para[0].textContent === "-Infinity") {
+    cal.style.webkitFilter = "blur(7px)";
+    let warBac = document.createElement("div");
+    warBac.style.position = "absolute";
+    warBac.style.width = "100%";
+    warBac.style.height = "100vh";
+    warBac.style.backgroundColor = "rgba(0,0,0,0.7)";
+    let warning = document.createElement("h1");
+    warning.textContent = "You broke Math, from this path there is no turning back";
+    warning.style.position = "absolute";
+    warning.style.color = "red";
+    warning.style.textAlign = "center";
+    body.appendChild(warBac);
+    body.appendChild(warning);
+  }
+});
+
 
 footer.addEventListener("click", () => {
   footer.textContent = "@_@";
